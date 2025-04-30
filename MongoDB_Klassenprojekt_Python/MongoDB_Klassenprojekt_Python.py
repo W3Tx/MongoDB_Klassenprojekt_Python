d1e60047158e123db5b73072e214c44e33802a35
W3Tx <foto.martindkawan@hotmail.com>
2025-04-30 10:00:27 +0200

    Update MongoDB_Klassenprojekt_Python.py

--- MongoDB_Klassenprojekt_Python/MongoDB_Klassenprojekt_Python.py
+++ MongoDB_Klassenprojekt_Python/MongoDB_Klassenprojekt_Python.py
@@ -1,7 +1,30 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+database = None
+
 # Hauptprogrammcode
 def main(): 
     # Schreib hier weiter 
-    print("Test");
+    print("Verbingung zu MongoDB")
+    list_databases()
+    select_or_create_database()
+
+
+def list_databases():
+    databases = client.list_database_names()
+    print("\nVerfügbare Datenbanken")
+
+    for db in databases: 
+        print(f"- {db}")
+
+
+def select_or_create_database():
+    global database
+    db_name = input("\nGeben Sie den Namen der Datenbank ein: ").strip()
+    database = client[db_name] 
+    print(f"Datenbank '{db_name}' ausgewählt (oder wird bei Bedarf erstellt)")
+
 
 # Startpunkt
 if __name__ == "__main__":
